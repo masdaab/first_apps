106dc3f47a5301b200003bdf309a7843a7659130
zinux <zinux@zinux-0S.(none)>
2013-11-05 06:58:55 +0700

    init git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApps::Application.config.secret_key_base = '6a641998eda113d0ba2bf0153f362e6f07dacc971cc8333e3cff488a183726ed088c8740470e1f123b18eb6cbd57ddf63b39f5d38ecbd1ad75886a2de91d835e'
